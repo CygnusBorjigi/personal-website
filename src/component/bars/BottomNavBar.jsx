a532ff439bceedb1a5f5f427967f7f6dba7fb459
SomeoneSomewhere <cygnusborjigin@gmail.com>
2022-05-13 13:31:21 -0400

    fixed personal website URL and cleaned directory

--- src/component/bars/BottomNavBar.jsx
+++ src/component/bars/BottomNavBar.jsx
@@ -5,7 +5,7 @@ import instagram from '../svgs/instagram.svg';
 const BottomBar = () => {
 	return (
 		<div className="static bottom-4 w-screen h-8 flex flex-cols justify-center">
-			<a href="https://github.com/CygnusBorjigi" className="h-8 w-8 mb-8"><img src={gitHub} alt={'github link'}/></a>
+			<a href="https://github.com/CygnusBorjigin" className="h-8 w-8 mb-8"><img src={gitHub} alt={'github link'}/></a>
 			<a href="https://www.instagram.com/cygnus__borjigin/" className="h-8 w-8 mb-8 ml-4"><img src={instagram} alt={'instagram link'} /></a>
 		</div>
 	);
